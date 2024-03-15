819646b68459e463602523a6eeab67ddebb5e429
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2024-03-15 19:41:02 -0300

    refactor: pratica 3

--- haskell/rel_velocidade.hs
+++ haskell/rel_velocidade.hs
@@ -1,19 +1,22 @@
-listAbaixo :: [Int] -> [Int]
-listAbaixo velocidades = [v | v <- velocidades, v <= 80]
-  
-listTolerancia :: [Int] -> [Int]
-listTolerancia velocidades = [v | v <- velocidades, v > 80 && v <= 88]
-  
-listMulta :: [Int] -> [Int]
-listMulta velocidades = [v | v <- velocidades, v > 88]
+filterVelocidades :: (Int -> Bool) -> [Int] -> [Int]
+filterVelocidades condicao velocidades = [v | v <- velocidades, condicao v]
 
+countAbaixo :: [Int] -> Int
+countAbaixo velocidades = length (filterVelocidades (<= 80) velocidades)
+
+countTolerancia :: [Int] -> Int
+countTolerancia velocidades = length (filterVelocidades (\v -> v > 80 && v <= 88) velocidades)
+
+countMulta :: [Int] -> Int
+countMulta velocidades = length (filterVelocidades (> 88) velocidades)
 
 relVelocidade :: [Int] -> String
 relVelocidade velocidades =
-  "=== Relatorio ===\n" ++ show (length (listAbaixo velocidades)) ++ " limite\n" ++
-  show (length (listTolerancia velocidades)) ++ " tolerancia\n" ++
-  show (length (listMulta velocidades)) ++ " multa\n"
+  "=== Relatorio ===\n" ++ show (countAbaixo velocidades) ++ " limite\n" ++
+  show (countTolerancia velocidades) ++ " tolerancia\n" ++
+  show (countMulta velocidades) ++ " multa\n"
 
 main :: IO ()
 main = do
   putStrLn (relVelocidade [55, 60, 80, 81, 88, 90])
+  -- ajustar entrada
