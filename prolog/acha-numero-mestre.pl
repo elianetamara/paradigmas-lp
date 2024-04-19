c2770984ba152a2455dd51126c2417b1b35e1789
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2024-04-19 15:10:45 -0300

    refactor: pratica 4 prolog

--- prolog/acha-numero-mestre.pl
+++ prolog/acha-numero-mestre.pl
@@ -1,33 +1,18 @@
-compativel(_, _, [], []).
+divisor_ou_digito(H, MESTRE) :-
+    (H mod MESTRE =:= 0 ;   H mod 10 =:= MESTRE).
 
-compativel(MESTRE, MESTRE,  [H | T] , [-1 | TOUTPUT]) :-
-    divisivel(H, MESTRE),
-    compativel(MESTRE, MESTRE, T, TOUTPUT).
-
-compativel(MESTRE, MESTRE,  [H | T] , [-1 | TOUTPUT]) :-
-    digito_final(H, MESTRE),
-    compativel(MESTRE, MESTRE, T, TOUTPUT).
-
-compativel(MESTRE, MESTRE, [H|T], [H|TOUTPUT]) :-
-    \+ (divisivel(H, MESTRE); digito_final(H, MESTRE)),
-    compativel(MESTRE, MESTRE, T, TOUTPUT).
-
-divisivel(NUMERO, MESTRE) :-
-    NUMERO mod MESTRE =:= 0.
-
-digito_final(NUMERO, MESTRE) :-
-    NUMERO mod 10 =:= MESTRE.
-
-valida_mestre(MESTRE, _) :-
-    between(1, 9, MESTRE), !.
-valida_mestre(_, LISTA) :-
-    writeln(LISTA), halt.
+compativel(_, [], []).
+compativel(MESTRE, [H|T], [-1|T2]) :-
+    divisor_ou_digito(H, MESTRE),
+    compativel(MESTRE, T, T2).
+compativel(MESTRE, [H|T], [H|T2]) :-
+    \+ divisor_ou_digito(H, MESTRE),
+    compativel(MESTRE, T, T2).
 
 main :-
     read(LISTA),
     read(MESTRE),
-    valida_mestre(MESTRE, LISTA),
-    compativel(MESTRE, MESTRE, LISTA, OUTPUT),
-    writeln(OUTPUT),
+    (between(1, 9, MESTRE) ->
+    compativel(MESTRE, LISTA, OUTPUT), writeln(OUTPUT)
+    ; writeln(LISTA)),
     halt.
-
